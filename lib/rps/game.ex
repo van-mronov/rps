222e61f0ab22e0378b9884a4a4a952cf0e7ed262
Ivan Mironov <ivan.mironov@gmail.com>
2018-05-06 21:44:26 +0700

    Implement basic game logic

--- lib/rps/game.ex
+++ lib/rps/game.ex
@@ -1,15 +1,72 @@
 defmodule RPS.Game do
 
-  @enforce_keys [:rounds]
-  defstruct rounds: nil, scores: %{}, result: nil
-
   alias RPS.{Game, Round}
 
+  defstruct current_round: %Round{}, rounds: [], first_player_score: 0, second_player_score: 0, result: nil
+
+  @duration 10
+
   @doc """
-  Creates a game with `duration` rounds.
+  Creates a new game.
   """
-  def new(duration) when is_integer(duration) do
-    rounds = for _round <- 1..duration, do: %Round{}
-    %Game{rounds: rounds}
+  def new do
+    %Game{}
+  end
+
+  def first_player_choice(game, choice) do
+    update_round_with_player_choice(game, :first, choice)
+  end
+
+  def second_player_choice(game, choice) do
+    game
+    |> update_round_with_player_choice(:second, choice)
+    |> update_round_result()
+    |> update_scores()
+    |> finish_round()
+    |> maybe_update_result()
+  end
+
+  defp update_round_with_player_choice(%Game{current_round: round} = game, player_position, choice) do
+    %{game | current_round: Round.player_choice(round, player_position, choice)}
+  end
+
+  defp update_round_result(%Game{current_round: round} = game) do
+    %{game | current_round: Round.update_result(round)}
+  end
+
+  defp update_scores(%Game{current_round: %Round{result: :first}, first_player_score: score} = game) do
+    %{game | first_player_score: score + 1}
+  end
+
+  defp update_scores(%Game{current_round: %Round{result: :second}, second_player_score: score} = game) do
+    %{game | second_player_score: score + 1}
+  end
+
+  defp update_scores(game) do
+    game
+  end
+
+  defp finish_round(%Game{current_round: round, rounds: rounds} = game) when length(rounds) == @duration - 1 do
+    %{game | current_round: nil, rounds: [round | rounds]}
+  end
+
+  defp finish_round(%Game{current_round: round, rounds: rounds} = game) do
+    %{game | current_round: %Round{}, rounds: [round | rounds]}
+  end
+
+  defp maybe_update_result(%Game{rounds: rounds} = game) when length(rounds) < @duration do
+    game
+  end
+
+  defp maybe_update_result(%Game{first_player_score: score, second_player_score: score} = game) do
+    %{game | result: :draw}
+  end
+
+  defp maybe_update_result(%Game{first_player_score: first_player_score, second_player_score: second_player_score} = game) when first_player_score > second_player_score do
+    %{game | result: :first}
+  end
+
+  defp maybe_update_result(game) do
+    %{game | result: :second}
   end
 end
--- lib/rps/round.ex
+++ lib/rps/round.ex
@@ -1,3 +1,28 @@
 defmodule RPS.Round do
-  defstruct player1_choice: nil, player2_choice: nil, result: nil
+  defstruct first_player_choice: nil, second_player_choice: nil, result: nil
+
+  alias RPS.Round
+
+  def player_choice(round, :first, choice) do
+    %{round | first_player_choice: choice}
+  end
+
+  def player_choice(round, :second, choice) do
+    %{round | second_player_choice: choice}
+  end
+
+  def update_result(%Round{first_player_choice: choice, second_player_choice: choice} = round),
+    do: %{round | result: :draw}
+  def update_result(%Round{first_player_choice: :rock, second_player_choice: :paper} = round),
+    do: %{round | result: :second}
+  def update_result(%Round{first_player_choice: :rock, second_player_choice: :scissors} = round),
+    do: %{round | result: :first}
+  def update_result(%Round{first_player_choice: :paper, second_player_choice: :rock} = round),
+    do: %{round | result: :first}
+  def update_result(%Round{first_player_choice: :paper, second_player_choice: :scissors} = round),
+    do: %{round | result: :second}
+  def update_result(%Round{first_player_choice: :scissors, second_player_choice: :rock} = round),
+    do: %{round | result: :second}
+  def update_result(%Round{first_player_choice: :scissors, second_player_choice: :paper} = round),
+    do: %{round | result: :first}
 end
