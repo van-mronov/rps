f4a10f0e9128e753024a7d825e1e655ce541dd7e
Ivan Mironov <ivan.mironov@gmail.com>
2018-05-07 16:53:43 +0700

    Remove dead code

--- lib/rps.ex
+++ lib/rps.ex
@@ -2,17 +2,4 @@ defmodule RPS do
   @moduledoc """
   Documentation for RPS.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> RPS.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/rps_test.exs
+++ test/rps_test.exs
@@ -1,8 +1,4 @@
 defmodule RPSTest do
   use ExUnit.Case
   doctest RPS
-
-  test "greets the world" do
-    assert RPS.hello() == :world
-  end
 end
