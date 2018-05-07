baee5fa8983e0be2e6501877f07de9e5ce7b603d
Ivan Mironov <ivan.mironov@gmail.com>
2018-05-07 17:11:50 +0700

    Add scissors beats paper tests

--- test/rps/round_test.exs
+++ test/rps/round_test.exs
@@ -11,7 +11,7 @@ defmodule RPS.RoundTest do
   end
 
   describe "rock beats scissors in update_result/1 and" do
-    test "result is `:first` if first player chose `:rock`" do
+    test "result is `:first` if the first player chose `:rock`" do
       round =
         Round.new()
         |> Round.player_choice(:first, :rock)
@@ -21,11 +21,33 @@ defmodule RPS.RoundTest do
       assert round.result == :first
     end
 
-    test "result is `:second` if second player chose `:rock`" do
+    test "result is `:second` if the second player chose `:rock`" do
       round =
         Round.new()
         |> Round.player_choice(:first, :scissors)
         |> Round.player_choice(:second, :rock)
+        |> Round.update_result()
+
+      assert round.result == :second
+    end
+  end
+
+  describe "scissors beats paper in update_result/1 and" do
+    test "result is `:first` if the first player chose `:scissors`" do
+      round =
+        Round.new()
+        |> Round.player_choice(:first, :scissors)
+        |> Round.player_choice(:second, :paper)
+        |> Round.update_result()
+
+      assert round.result == :first
+    end
+
+    test "result is `:second` if the second player chose `:scissors`" do
+      round =
+        Round.new()
+        |> Round.player_choice(:first, :paper)
+        |> Round.player_choice(:second, :scissors)
         |> Round.update_result()
 
       assert round.result == :second
