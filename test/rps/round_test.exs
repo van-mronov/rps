e29598f7a2c6999c5b22e7f040a2e897e634b247
Ivan Mironov <ivan.mironov@gmail.com>
2018-05-07 17:07:15 +0700

    Add rock beats scissors tests

--- test/rps/round_test.exs
+++ test/rps/round_test.exs
@@ -9,4 +9,26 @@ defmodule RPS.RoundTest do
       assert Round.new() == %Round{first_player_choice: nil, second_player_choice: nil, result: nil}
     end
   end
+
+  describe "rock beats scissors in update_result/1 and" do
+    test "result is `:first` if first player chose `:rock`" do
+      round =
+        Round.new()
+        |> Round.player_choice(:first, :rock)
+        |> Round.player_choice(:second, :scissors)
+        |> Round.update_result()
+
+      assert round.result == :first
+    end
+
+    test "result is `:second` if second player chose `:rock`" do
+      round =
+        Round.new()
+        |> Round.player_choice(:first, :scissors)
+        |> Round.player_choice(:second, :rock)
+        |> Round.update_result()
+
+      assert round.result == :second
+    end
+  end
 end
