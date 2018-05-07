1d6707b9304a9970d9070a324bb3c197f37814ce
Ivan Mironov <ivan.mironov@gmail.com>
2018-05-07 16:58:08 +0700

    Add test new/1 returns a blank round

--- test/rps/round_test.exs
+++ test/rps/round_test.exs
@@ -1,4 +1,12 @@
 defmodule RPS.RoundTest do
   use ExUnit.Case
   doctest RPS.Round
+
+  alias RPS.Round
+
+  describe "new/0" do
+    test "returns a blank round" do
+      assert Round.new() == %Round{first_player_choice: nil, second_player_choice: nil, result: nil}
+    end
+  end
 end
