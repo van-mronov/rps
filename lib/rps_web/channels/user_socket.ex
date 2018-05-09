b4b3cf2406573f0c0df842f4f6b5e61d125cebd5
Ivan Mironov <ivan.mironov@gmail.com>
2018-05-09 23:20:12 +0700

    Add user socket connect tests

--- lib/rps_web/channels/user_socket.ex
+++ lib/rps_web/channels/user_socket.ex
@@ -1,26 +1,21 @@
 defmodule RpsWeb.UserSocket do
   use Phoenix.Socket
+  require Logger
 
   ## Channels
   # channel "room:*", RpsWeb.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  def connect(%{"access_token" => token}, socket) do
+    case Phauxth.Token.verify(socket, token, 86400) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      error ->
+        error |> inspect() |> Logger.warn()
+        :error
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
---
+++ test/rps_web/channels/user_socket_test.exs
@@ -0,0 +1,23 @@
+defmodule RpsWeb.UserSocketTest do
+  use RpsWeb.ChannelCase, async: true
+
+  import RpsWeb.AuthCase
+  alias RpsWeb.UserSocket
+
+  setup do
+    user = add_user("Ivan")
+    token = Phauxth.Token.sign(RpsWeb.Endpoint, user.id)
+    {:ok, user: user, token: token}
+  end
+
+  describe "connect/2" do
+    test "disable connection for unauthenticated users" do
+      assert connect(UserSocket, %{"access_token" => "invalid_token"}) == :error
+    end
+
+    test "assigns user_id for authenticated users", %{user: user, token: token} do
+      {:ok, socket} = connect(UserSocket, %{"access_token" => token})
+      assert socket.assigns.user_id == user.id
+    end
+  end
+end
