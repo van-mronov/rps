5e0a80410f325a1617ff87fea4279db4b7702db1
Ivan Mironov <ivan.mironov@gmail.com>
2018-05-10 17:05:43 +0700

    Pushes the current game info after join

--- lib/rps_web/channels/game_channel.ex
+++ lib/rps_web/channels/game_channel.ex
@@ -5,9 +5,15 @@ defmodule RpsWeb.GameChannel do
 
   def join("game:" <> game_name, _params, socket) do
     if GameServer.alive?(game_name) do
+      send(self(), {:after_join, game_name})
       {:ok, socket}
     else
       {:error, %{reason: "Game does not exist"}}
     end
+  end
+
+  def handle_info({:after_join, game_name}, socket) do
+    push(socket, "game_info", GameServer.info(game_name))
+    {:noreply, socket}
   end
 end
--- test/rps_web/channels/game_channel_test.exs
+++ test/rps_web/channels/game_channel_test.exs
@@ -2,13 +2,16 @@ defmodule RpsWeb.GameChannelTest do
   use RpsWeb.ChannelCase
 
   import RpsWeb.AuthCase
+  alias Rps.GameServer
+  alias RpsWeb.GameChannel
 
   setup do
     user = add_user("Ivan")
     token = Phauxth.Token.sign(RpsWeb.Endpoint, user.id)
     {:ok, socket} = connect(RpsWeb.UserSocket, %{"access_token" => token})
     {:ok, game_name} = Rps.start_game(user.id)
-    {:ok, user: user, token: token, socket: socket, game_name: game_name}
+    topic = "game:" <> game_name
+    {:ok, user: user, token: token, socket: socket, game_name: game_name, topic: topic}
   end
 
   describe "join" do
@@ -16,9 +19,15 @@ defmodule RpsWeb.GameChannelTest do
       assert join(socket, "game:invalid_game_name") == {:error, %{reason: "Game does not exist"}}
     end
 
-    test "replies with status ok for creator of the game", context do
-      {:ok, %{}, socket} = join(context.socket, "game:" <> context.game_name)
+    test "replies with status ok for creator of the game", %{socket: socket, topic: topic} do
+      {:ok, _reply, socket} = join(socket, topic)
       assert socket.joined
+    end
+
+    test "pushes the current game info", context do
+      {:ok, _reply, _socket} = subscribe_and_join(context.socket, GameChannel, context.topic, %{})
+      game_info = GameServer.info(context.game_name)
+      assert_push("game_info", ^game_info)
     end
   end
 end
